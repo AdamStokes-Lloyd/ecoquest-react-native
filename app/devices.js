b3fec9604972b68571db7eee985b9e5fb10bed9e
AdamStokes-Lloyd <adamstokeslloyd@gmail.com>
2025-07-15 22:59:43 +0000

    added stylesheet compnents to devices and characters files

--- app/devices.js
+++ app/devices.js
@@ -8,3 +8,7 @@ export default function DevicesScreen() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 20, fontWeight: 'bold' },
+});
