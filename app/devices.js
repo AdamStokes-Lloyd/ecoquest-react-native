d2494c4945882b47e5785153b054f704e536e2f6
AdamStokes-Lloyd <adamstokeslloyd@gmail.com>
2025-07-15 02:28:18 +0000

    Correctly reinstalled 'Apps' folder as 'apps', deleted previous folder, reinitialised all code, numerous new npm installs, added metro.config workaround, attempting to resolve unable to locate metro bundler plug in error

--- app/devices.js
+++ app/devices.js
@@ -8,8 +8,3 @@ export default function DevicesScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 20, fontWeight: 'bold' },
-});
